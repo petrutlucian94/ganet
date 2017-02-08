a9ac9554aa57e7f3be36d257b28bd9abb6aacbf4
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2017-02-08 13:22:40 +0200

    Raise default mutation rate

--- ganet/conf.py
+++ ganet/conf.py
@@ -1,6 +1,6 @@
 CONF = {
     'community_score_mean_order': 0.5,
-    'mutation_rate': 0.01,
+    'mutation_rate': 0.05,
     'crossover_rate': 0.8,
     'elite_reproduction_rate': 0.1,
     'population_size': 300,
